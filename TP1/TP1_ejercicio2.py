a07c51ce5c403cc589c281ca4d5daadfa1e6206d
Eva <114450123+eovelar@users.noreply.github.com>
2024-09-15 18:39:06 -0300

    ejercicio 2

---
+++ TP1/TP1_ejercicio2.py
@@ -0,0 +1,7 @@
+"""
+Desarrollar una función que reciba tres números enteros positivos correspondientes
+al día, mes, año de una fecha y verifique si corresponden a una fecha válida. Debe
+tenerse en cuenta la cantidad de días de cada mes, incluyendo los años bisiestos.
+Devolver True o False según la fecha sea correcta o no. Realizar también un
+programa para verificar el comportamiento de la función.
+"""
