dbdf3fd9694eefc4a25013375f07fd9177f26b68
Eva <114450123+eovelar@users.noreply.github.com>
2024-09-16 17:44:26 -0300

    ejercicio 2 (me comí la consigna)

--- TP1/TP1_ejercicio2.py
+++ TP1/TP1_ejercicio2.py
@@ -1,3 +1,11 @@
+"""
+Desarrollar una función que reciba tres números enteros positivos correspondientes
+al día, mes, año de una fecha y verifique si corresponden a una fecha válida. Debe
+tenerse en cuenta la cantidad de días de cada mes, incluyendo los años bisiestos.
+Devolver True o False según la fecha sea correcta o no. Realizar también un
+programa para verificar el comportamiento de la función.
+"""
+
 def anio_bisiesto(anio):
     """
     Verifica si un año es bisiesto.
