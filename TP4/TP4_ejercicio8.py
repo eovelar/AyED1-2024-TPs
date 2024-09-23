228260da89ea8fcd4f4577e2244ff1a47d4d8755
Eva <114450123+eovelar@users.noreply.github.com>
2024-09-22 21:51:37 -0300

    TP4_ejercicio8.py

--- TP4/TP4_ejercicio8.py
+++ TP4/TP4_ejercicio8.py
@@ -2,3 +2,28 @@
 Desarrollar una función que devuelva una subcadena con los últimos N caracteres
 de una cadena dada. La cadena y el valor de N se pasan como parámetros.
 """
+
+def ultimos_n_caracteres(cadena, n):
+    """
+    Devuelve una subcadena con los últimos N caracteres de una cadena dada.
+
+    Parámetros:
+        cadena (str): La cadena original.
+        n (int): El número de caracteres a devolver
+
+    Returns:
+        str: La subcadena con los últimos N caracteres
+    """
+    if n > len(cadena):
+        return "Error: N es mayor que la longitud de la cadena"
+    else:
+        return cadena[-n:]
+
+def main():
+    cadena = input("Ingrese una cadena: ")
+    n = int(input("Ingrese el número de caracteres a devolver: "))
+    resultado = ultimos_n_caracteres(cadena, n)
+    print("Los últimos", n, "caracteres de la cadena son:", resultado)
+
+if __name__ == "__main__":
+    main()
